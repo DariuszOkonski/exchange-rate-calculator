0f48e9ef8a14ce173664405cd8ac0729d908bd7a
Dariusz <darek200180@gmail.com>
2020-11-13 21:42:09 +0000

    remove file items.json

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 function calculate() {
-    fetch('items.json')
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err.message))
+    console.log('calculate')
 }
 
 calculate();
